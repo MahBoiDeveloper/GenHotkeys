4a5f66de37d5a9a703b09f25f0012e676828ae75
MahBoiDeveloper <mah.boi.developer@gmail.com>
2026-02-23 15:41:12 +0300

    Add optional parameter for l10n functions

--- src/Extensions/L10NExt.cpp
+++ src/Extensions/L10NExt.cpp
@@ -1,7 +1,8 @@
 #include <QCoreApplication>
+#include "StringExt.hpp"
 #include "L10NExt.hpp"
 
-QString L10NExt::l10n(const char*         string) { return QCoreApplication::translate("QObject", QString(string).toUtf8().constData()); }
-QString L10NExt::l10n(const QString&      string) { return QCoreApplication::translate("QObject", string.toUtf8().constData()); }
-QString L10NExt::l10n(const std::string&  string) { return QCoreApplication::translate("QObject", QString::fromStdString(string).toUtf8().constData()); }
-QString L10NExt::l10n(const std::wstring& string) { return QCoreApplication::translate("QObject", QString::fromStdWString(string).toUtf8().constData()); }
+QString L10NExt::l10n(const char*         string, const QString& object) { return QCoreApplication::translate(object.toStdString().c_str(), string); }
+QString L10NExt::l10n(const QString&      string, const QString& object) { return QCoreApplication::translate(object.toStdString().c_str(), StringExt::ToQString(string).toStdString().c_str()); }
+QString L10NExt::l10n(const std::string&  string, const QString& object) { return QCoreApplication::translate(object.toStdString().c_str(), StringExt::ToQString(string).toStdString().c_str()); }
+QString L10NExt::l10n(const std::wstring& string, const QString& object) { return QCoreApplication::translate(object.toStdString().c_str(), StringExt::ToQString(string).toStdString().c_str()); }
--- src/Extensions/L10NExt.hpp
+++ src/Extensions/L10NExt.hpp
@@ -2,17 +2,17 @@
 #include <QString>
 #include <QTranslator>
 
-/// @brief Shortcut for StringExt::l10n()
-#define L10N(x) L10NExt::l10n(x)
+/// @brief Shortcut for StringExt::l10n(x, "QObject")
+#define L10N(x)    L10NExt::l10n(x)
 
 namespace L10NExt
 {
     /// @brief Shortcut for QCoreApplication::translate("QObject", str)
-    QString l10n(const char*         string);
+    QString l10n(const char*         string, const QString& object = "QObject");
     /// @brief Shortcut for QCoreApplication::translate("QObject", str)
-    QString l10n(const QString&      string);
+    QString l10n(const QString&      string, const QString& object = "QObject");
     /// @brief Shortcut for QCoreApplication::translate("QObject", str)
-    QString l10n(const std::string&  string);
+    QString l10n(const std::string&  string, const QString& object = "QObject");
     /// @brief Shortcut for QCoreApplication::translate("QObject", str)
-    QString l10n(const std::wstring& string);
+    QString l10n(const std::wstring& string, const QString& object = "QObject");
 }
